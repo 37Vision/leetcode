043e06ef97f32481b9f113699cddfb905e1e34f9
Darkness <32087505+37Vision@users.noreply.github.com>
2018-05-02 23:26:52 +0800

    #83remove-duplicates-from-sorted-list
    很简单的思路 挨个比较就行

---
+++ #83remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if(head == NULL)
+        return head;
+    struct ListNode *p = head,*temp = p->next;
+    while(temp != NULL)
+    {
+        if(p->val == temp->val)
+        {
+            p->next = temp->next;
+            free(temp);
+        }
+        else
+            p = p->next;	//如果当前值和下一个值不等时移动指针P
+        if(p != NULL)
+            temp = p->next;
+        else
+            temp = NULL;
+    }
+    return head;
+}
