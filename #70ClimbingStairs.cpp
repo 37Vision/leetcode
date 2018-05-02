ff2ca26115a70856aa0007829919b22ebdd994ee
Darkness <32087505+37Vision@users.noreply.github.com>
2018-05-02 23:03:15 +0800

    #70ClimbingStairs
    找出状态转移方程 简单的dp问题

---
+++ #70ClimbingStairs.cpp
@@ -0,0 +1,10 @@
+﻿int climbStairs(int n) {
+    int result[50];
+    result[2] = 2;
+    result[1] = 1;
+    for(int i = 3;i<=n;i++)
+    {
+        result[i] = result[i-1] + result[i-2];  //状态转移方程
+    }
+    return result[n];
+}
