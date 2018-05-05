6c451b4d50e73ce19ffb53f103b635178991a6d0
Darkness <32087505+37Vision@users.noreply.github.com>
2018-05-05 18:35:36 +0800

    #104maximum-depth-of-binary-tree
    简单递归 NULL深度为0

---
+++ #104maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,15 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int maxDepth(struct TreeNode* root) {
+    if(root == NULL)
+        return 0;
+    int l = maxDepth(root->left); //左子树深度 
+    int r =  maxDepth(root->right);//右子树深度 
+    return l > r ? l + 1:r + 1;
+}
