1f7362e458077db0166050d83db9081cb438fd27
Darkness <32087505+37Vision@users.noreply.github.com>
2018-05-22 19:33:34 +0800

    #169majority-element
    最大投票算法

---
+++ #169majority-element.cpp
@@ -0,0 +1,17 @@
+//Moore's voting最大投票算法
+int majorityElement(int* nums, int numsSize) { 
+    int count = 0,result = 0;
+    for(int i = 0;i<numsSize;i++)
+    {
+       if(count == 0) 
+       {
+           result = nums[i];
+           count++;
+       }
+        else if(nums[i] == result)
+            count++;
+        else
+            count--;
+    }
+    return result;
+}
