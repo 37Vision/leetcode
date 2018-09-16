5072b04bf3830685503c354f5d1d7e263fec8355
Darkness <32087505+37Vision@users.noreply.github.com>
2018-09-16 20:39:48 +0800

    #217重读元素
    用一个Map 时间复杂度O(n),空间复杂度O(n)

---
+++ 217.contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> hash;
+        for(int i = 0;i<nums.size();i++)
+        {
+            if(hash.find(nums[i]) != hash.end())
+                return true;
+            else
+                hash.insert(pair<int,int>(nums[i],1));
+        }
+        return false;
+    }
+};
