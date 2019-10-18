8f571e82f293463e9e2ddb4826d5b58b0dc34ba3
Darkness <32087505+37Vision@users.noreply.github.com>
2019-10-18 21:05:00 +0800

    动态规划问题

---
+++ #152maximum-product-subarray.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int len = nums.size();
+        int Min = nums[0],Max = nums[0],res = nums[0];//Min,Max分别代表以当前值结尾的最小子序列值和最大子序列值
+        for(int i = 1; i < len;i++)
+        {
+	//新加入值后，以新加入值结尾的最大值要么是最大值乘新加值，要么是最小值乘新加值，要么是新加值
+            int t = Min;
+            Min = min(min(Min * nums[i],Max * nums[i]),nums[i]);
+            Max = max(max(t * nums[i],Max * nums[i]),nums[i]);
+            if(Max > res)
+                res = Max;
+        }
+        return res;
+    }
+};
